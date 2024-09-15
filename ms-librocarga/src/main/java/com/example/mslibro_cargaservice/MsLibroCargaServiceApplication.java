7f03c3e156857b0352e190f42b71691068d4c4eb
ClaudioB12 <119316401+ClaudioB12@users.noreply.github.com>
2024-09-14 23:53:43 -0500

    librocarga

--- ms-librocarga/src/main/java/com/example/mslibro_cargaservice/MsLibroCargaServiceApplication.java
+++ ms-librocarga/src/main/java/com/example/mslibro_cargaservice/MsLibroCargaServiceApplication.java
@@ -18,7 +18,7 @@ public class MsLibroCargaServiceApplication {
 		return new OpenAPI().info(new Info()
 				.title("OPEN API MICROSERVICIO CLIENTE")
 				.version("0.0.1")
-				.description("servicio web cliente")
+				.description("servicio web librocarga")
 				.termsOfService("http://swagger.io/terms")
 				.license(new License().name("Apache 2.0").url("http://springdoc.org"))
 		);
