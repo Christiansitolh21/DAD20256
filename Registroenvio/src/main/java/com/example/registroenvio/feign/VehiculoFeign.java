d9bcadc0b69464ccf44132d79820dc2e9ceeb736
ClaudioB12 <119316401+ClaudioB12@users.noreply.github.com>
2024-10-20 00:03:22 -0500

    Resiliencia ,balanceo de carga y implementación de seguridad

--- Registroenvio/src/main/java/com/example/registroenvio/feign/VehiculoFeign.java
+++ Registroenvio/src/main/java/com/example/registroenvio/feign/VehiculoFeign.java
@@ -1,6 +1,7 @@
 package com.example.registroenvio.feign;
 
 import com.example.registroenvio.dto.VehiculoDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-vehiculo-service", path = "/vehiculo")
 public interface VehiculoFeign {
     @GetMapping("/{id}")
-    public ResponseEntity<VehiculoDto> getById(@PathVariable(required = true) Integer id);
+    @CircuitBreaker(name = "vehiculoListByIdCB", fallbackMethod = "vehiculoListById")
+    public ResponseEntity<VehiculoDto> getById(@PathVariable Integer id);
+    default ResponseEntity<VehiculoDto> vehiculoListById(Integer id, Exception e) {
+        return ResponseEntity.ok(new VehiculoDto());
+    }
 }
