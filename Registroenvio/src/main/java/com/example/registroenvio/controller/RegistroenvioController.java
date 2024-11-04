0d6b4da98a65671efd65f3f71fad2f062656411a
ClaudioB12 <119316401+ClaudioB12@users.noreply.github.com>
2024-11-04 08:24:51 -0500

    Act

--- Registroenvio/src/main/java/com/example/registroenvio/controller/RegistroenvioController.java
+++ Registroenvio/src/main/java/com/example/registroenvio/controller/RegistroenvioController.java
@@ -1,8 +1,15 @@
 package com.example.registroenvio.controller;
 
+import com.example.registroenvio.dto.ClienteDto;
+import com.example.registroenvio.dto.ErrorResponseDto;
+import com.example.registroenvio.dto.VehiculoDto;
+import com.example.registroenvio.entity.RegistroDetalle;
 import com.example.registroenvio.entity.Registroenvio;
+import com.example.registroenvio.feign.ClienteFeign;
+import com.example.registroenvio.feign.VehiculoFeign;
 import com.example.registroenvio.service.RegistroenvioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +21,10 @@ import java.util.List;
 public class RegistroenvioController {
     @Autowired
     private RegistroenvioService registroenvioService;
+    @Autowired
+    private ClienteFeign clienteFeign;
+    @Autowired
+    private VehiculoFeign vehiculoFeign;
 
     @GetMapping
     public ResponseEntity<List<Registroenvio>> listar(){
@@ -21,8 +32,23 @@ public class RegistroenvioController {
     }
 
     @PostMapping
-    public ResponseEntity<Registroenvio> guardar(@RequestBody Registroenvio registroenvio){
-        return ResponseEntity.ok(registroenvioService.guardar(registroenvio));
+    public ResponseEntity<?> guardar(@RequestBody Registroenvio registroenvio) {
+        ClienteDto clienteDto = clienteFeign.getById(registroenvio.getClienteId()).getBody();
+
+        if (clienteDto == null || clienteDto.getId() == null) {
+            String errorMessage = "Error: Registro no encontrado.";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDto(errorMessage));
+        }
+        for (RegistroDetalle registroDetalle: registroenvio.getRegistroDetalles()) {
+            VehiculoDto vehiculoDto = vehiculoFeign.getById(registroenvio.getVehiculoId()).getBody();
+
+            if (vehiculoDto == null || vehiculoDto.getId() == null) {
+                String errorMessage = "Error: producto no encontrado.";
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDto(errorMessage));
+            }
+        }
+        Registroenvio newRegistroenvio = registroenvioService.guardar(registroenvio);
+        return ResponseEntity.ok(newRegistroenvio);
     }
 
     @GetMapping("/{id}")
--- ms-gestionenvio/src/main/java/com/example/msgestionenvio/controller/GestionenvioController.java
+++ ms-gestionenvio/src/main/java/com/example/msgestionenvio/controller/GestionenvioController.java
@@ -1,12 +1,20 @@
 package com.example.msgestionenvio.controller;
 
+import com.example.msgestionenvio.dto.ErrorResponseDto;
+import com.example.msgestionenvio.dto.LibrocargaDto;
+import com.example.msgestionenvio.dto.RegistroenvioDto;
+import com.example.msgestionenvio.entity.GestionDetalle;
 import com.example.msgestionenvio.entity.Gestionenvio;
+import com.example.msgestionenvio.feign.LibrocargaFeign;
+import com.example.msgestionenvio.feign.RegistroenvioFeign;
 import com.example.msgestionenvio.service.GestionenvioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/gestionenvio")
@@ -14,14 +22,36 @@ public class GestionenvioController {
     @Autowired
     private GestionenvioService gestionenvioService;
 
+    @Autowired
+    private RegistroenvioFeign registroenvioFeign;
+
+    @Autowired
+    private LibrocargaFeign librocargaFeign;
+
     @GetMapping
     public ResponseEntity<List<Gestionenvio>> listar(){
         return ResponseEntity.ok(gestionenvioService.listar());
     }
 
+
     @PostMapping
-    public ResponseEntity<Gestionenvio> guardar(@RequestBody Gestionenvio gestionenvio){
-        return ResponseEntity.ok(gestionenvioService.guardar(gestionenvio));
+    public ResponseEntity<?> guardar(@RequestBody Gestionenvio gestionenvio) {
+        RegistroenvioDto registroenvioDto = registroenvioFeign.getById(gestionenvio.getRegistroenvioId()).getBody();
+
+        if (registroenvioDto == null || registroenvioDto.getId() == null) {
+            String errorMessage = "Error: Registro no encontrado.";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDto(errorMessage));
+        }
+        for (GestionDetalle gestionDetalle : gestionenvio.getGestionDetalles()) {
+            LibrocargaDto librocargaDto = librocargaFeign.getById(gestionenvio.getLibrocargaId()).getBody();
+
+            if (librocargaDto == null || librocargaDto.getId() == null) {
+                String errorMessage = "Error: producto no encontrado.";
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDto(errorMessage));
+            }
+        }
+        Gestionenvio newGestionenvio = gestionenvioService.guardar(gestionenvio);
+        return ResponseEntity.ok(newGestionenvio);
     }
 
     @GetMapping("/{id}")
