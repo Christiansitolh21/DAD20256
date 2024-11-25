733032b21aea581e0fd6d8c9460bd72d1b58fc4f
ClaudioB12 <119316401+ClaudioB12@users.noreply.github.com>
2024-11-24 23:14:06 -0500

    Frontend vehiculo 5.0

--- ms-vehiculo/src/main/java/com/example/msvehiculo/controller/VehiculoController.java
+++ ms-vehiculo/src/main/java/com/example/msvehiculo/controller/VehiculoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+@CrossOrigin(origins = "http://localhost:4200") // Habilitar CORS para Angular
 
 @RestController
 @RequestMapping("/vehiculo")
