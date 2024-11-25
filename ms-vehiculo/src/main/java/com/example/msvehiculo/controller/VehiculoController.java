8bb4e56fb0efd8c051d2b7611937285da0dd7277
ClaudioB12 <119316401+ClaudioB12@users.noreply.github.com>
2024-11-24 22:31:44 -0500

    Frontend vehiculo 2.0

--- ms-vehiculo/src/main/java/com/example/msvehiculo/controller/VehiculoController.java
+++ ms-vehiculo/src/main/java/com/example/msvehiculo/controller/VehiculoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/vehiculos")
+@RequestMapping("/vehiculo")
 public class VehiculoController {
 
     @Autowired
